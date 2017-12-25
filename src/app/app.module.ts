7f6fce2b81271a90b1371256e0021259a501d04e
Nacho Torrella <nachotorrella@gmail.com>
2017-12-25 10:40:07 +0100

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListQuestionsComponent } from './components/list-questions/list-questions.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListQuestionsComponent
   ],
   imports: [
     BrowserModule
